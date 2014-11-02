873b872c3387b4e630db7d59fce79542e59d6e85
Darcy Webber <darcy@quantifish.co.nz>
2014-11-02 19:45:12 +1300

    Finished sim fits

--- simulation/SimulateGrowth_v4.R
+++ simulation/SimulateGrowth_v4.R
@@ -26,7 +26,7 @@ sd_b <- c(0.21,0.19)
 gamma <- c(0.19,0.19)
 psi <- c(0.0000000002,0.0000000002)
 sd_obs <- c(0.083,0.083)
-sd_z <- c(0.05,0.05)
+sd_z <- c(0.0,0.0)
 sd_y <- c(0,0)
 
 Pars <- rbind(L0, bmean, sd_b, gamma, psi, sd_obs, sd_z, sd_y)
@@ -41,7 +41,7 @@ for (ss in 1:Ndesign)
 {
     #ATR_sim <- SimGrowth(ln_xdev=NULL, ln_ydev=ln_ydev, obs_err=TRUE, tvi_err=TRUE)$ATR_sim
     ATR_sim <- SimGrowth(ln_xdev = NULL, ln_ydev = NULL,
-                         obs_err = TRUE, tvi_err = TRUE, Pars, Nindiv)
+                         obs_err = TRUE, tvi_err = FALSE, Pars, Nindiv)
     #ATR_sim <- SimGrowth(ln_xdev=NULL, ln_ydev=NULL, obs_err=FALSE, tvi_err=TRUE)$ATR_sim
     #ATR_sim <- SimGrowth(ln_xdev=NULL, ln_ydev=NULL, obs_err=TRUE, tvi_err=FALSE)$ATR_sim
     #ATR_sim <- SimGrowth(ln_xdev=NULL, ln_ydev=NULL, obs_err=FALSE, tvi_err=FALSE, Input[[1]]$Parameters, Input[[1]]$Data)$ATR_sim
--- simulation/est/ATR_TMB.R
+++ simulation/est/ATR_TMB.R
@@ -8,35 +8,60 @@ rm(list = ls())
 # Load TMB
 require(TMB)
 
-compile("ATR.cpp")
+compile("../../estimation/ATR.cpp")
 
-for (Isim in 86:100)
+for (Isim in 1:100)
 {
     cat("\nStarting simulation", Isim, "...\n")
-    dyn.load(dynlib("ATR"))
+    dyn.load(dynlib("../../estimation/ATR"))
     # Data
     fname <- paste("../sims/sim", Isim, ".RData", sep = "")
     load(fname)
     ATR_mod <- sim$Sim
+    Options <- c("YearTF"=0, "AreaTF"=0, "IndivTF"=1, "IndivTimeTF"=0) #1st slot: 
     Nindiv <- nrow(ATR_mod)
     # Make AD object
-    Data <- list(iAge1 = ATR_mod[1:Nindiv,'Age1'], iLiberty = ATR_mod[1:Nindiv,'Liberty'],
-                 Length1 = ATR_mod[1:Nindiv,'Length1'], Length2 = ATR_mod[1:Nindiv,'Length2'],
-                 Sex = ATR_mod[1:Nindiv,'Sex'],
-                 Time0 = ATR_mod[1:Nindiv,'Time0'], Time1 = ATR_mod[1:Nindiv,'Time1'],
-                 Year0 = ATR_mod[1:Nindiv,'Year0'], Year1 = ATR_mod[1:Nindiv,'Year1'],
-                 Area1 = ATR_mod[1:Nindiv,'Area1'])
+    Data <- list(Options=Options, iAge1 = ATR_mod[1:Nindiv,'Age1'], iLiberty = ATR_mod[1:Nindiv,'Liberty'],
+             Length1 = ATR_mod[1:Nindiv,'Length1'], Length2 = ATR_mod[1:Nindiv,'Length2'],
+             Sex = ATR_mod[1:Nindiv,'Sex'],
+             Time0 = ATR_mod[1:Nindiv,'Time0'], Time1 = ATR_mod[1:Nindiv,'Time1'],
+             Year0 = ATR_mod[1:Nindiv,'Year0'], Year1 = ATR_mod[1:Nindiv,'Year1'],
+             Area1 = ATR_mod[1:Nindiv,'Area1'])
     Nyears <- 40
     Nareas <- length(unique(ATR_mod$Area1))
     Params <- list(ln_gamma = log(10000), logit_psi = qlogis(0.2), ln_L0 = rep(log(1), 2),
-                   ln_bmean = rep(log(0.2), 2), ln_bdev = rep(0, Nindiv), ln_sd_bdev = c(log(0.01), log(0.01)),
-                   ln_sd_obs = log(20),
-                   z1 = rep(0, Nindiv), z2 = rep(0, Nindiv), ln_sd_z = log(0.1),
-                   ln_ydev = rep(0, Nyears), ln_sd_ydev = log(0.01),
-                   ln_xdev = rep(0, Nareas), ln_sd_xdev = log(0.01))
-    obj <- MakeADFun(data = Data, parameters = Params,
-                     map = list(ln_ydev = factor(rep(NA, Nyears)), ln_sd_ydev = factor(NA), ln_xdev = factor(rep(NA, Nareas)), ln_sd_xdev = factor(NA)),
-                     random = c("ln_bdev", "z1", "z2"))
+               ln_bmean = rep(log(0.2), 2), ln_bdev = rep(0, Nindiv), ln_sd_bdev = c(log(0.01), log(0.01)),
+               ln_sd_obs = log(20), z1 = rep(0, Nindiv), z2 = rep(0, Nindiv), ln_sd_z = log(0.1),
+               ln_ydev = rep(0, Nyears), ln_sd_ydev = log(0.01),
+               ln_xdev = rep(0, Nareas), ln_sd_xdev = log(0.01))
+Random = NULL
+Map <- list()
+if(Options[1]==0){
+  Map[["ln_ydev"]] = factor(rep(NA, Nyears))
+  Map[["ln_sd_ydev"]] = factor(NA)
+}else{
+  Random = c(Random, "ln_ydev")
+} 
+if(Options[2]==0){
+  Map[["ln_sd_xdev"]]=factor(NA)
+  Map[["ln_xdev"]]=factor(rep(NA,length(Params$ln_xdev))) 
+}else{
+  Random = c(Random, "ln_xdev")
+}
+if(Options[3]==0){
+  Map[["ln_bdev"]]=factor(rep(NA,length(Params$ln_bdev)))
+  Map[["ln_sd_bdev"]]=factor(rep(NA,2)) 
+}else{
+  Random = c(Random, "ln_bdev")
+}
+if(Options[4]==0){
+  Map[["z2"]]=factor(rep(NA,length(Params$ln_bdev)))
+  Map[["z1"]]=factor(rep(NA,length(Params$z1)))
+  Map[["ln_sd_z"]]=factor(NA)  
+}else{
+  Random = c(Random, "z1", "z2")
+}
+obj <- MakeADFun(data = Data, parameters = Params, map = Map, random = Random, inner.control=list(maxit=50))
     # Run model
     newtonOption(smartsearch = TRUE)
     obj$fn(obj$par)
@@ -45,12 +70,19 @@ for (Isim in 86:100)
     obj$env$inner.control$trace <- FALSE
     obj$env$silent <- TRUE
     obj$hessian <- TRUE
-    #obj$env$inner.control$step.tol <- 1e-12 # Default : 1e-8 # Change in parameters limit inner optimization
-    #obj$env$inner.control$tol10 <- 1e-8     # Default : 1e-3 # Change in pen.like limit inner optimization
-    #obj$env$inner.control$grad.tol <- 1e-12 # Default : 1e-8 # Maximum gradient limit inner optimization
+    ConvergeTol <- 1 # 1:Normal; 2:Strong
+    Upr = rep(Inf, length(obj$par))
+  Upr[match("logit_psi",names(obj$par))] = qlogis(0.999)
+Lwr = rep(-Inf, length(obj$par))
+  Lwr[match("logit_psi",names(obj$par))] = qlogis(0.001)
+  Lwr[match("ln_sd_z",names(obj$par))] = log(0.001)
+  Lwr[match("ln_sd_xdev",names(obj$par))] = log(0.001)
+  Lwr[match("ln_sd_bdev",names(obj$par))] = log(0.001)
+
     doFit <- function()
     {
         opt <- nlminb(start = obj$par, objective = obj$fn, control = list(eval.max = 1e4, iter.max = 1e4))
+        opt[["final_gradient"]] = obj$gr(opt$par)
         Report <- sdreport(obj)
         # Append Report to sim and save as .RData file
         sim$obj <- obj
@@ -64,5 +96,5 @@ for (Isim in 86:100)
         save(sim, file = fname)
     }
     tryCatch(doFit(), error = function(e) cat("Error in simulation", Isim, "\n"), finally = cat("Simulation", Isim, "done\n"))
-    dyn.unload(dynlib("ATR"))
+    dyn.unload(dynlib("../../estimation/ATR"))
 }
